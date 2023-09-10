ceac18a8ecba1831d7c8ff24e6a541e74360105e
paschal-ike <paschalpaul268@gmail.com>
2023-09-10 18:22:24 +0100

    second commit

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,23 +1,33 @@
 from django.shortcuts import render
 from django.views.generic import View
 from django.http import JsonResponse
+import datetime
 
 class ApiView(View):
     def get(self, request, *args, **kwargs):
         slack_name = request.GET.get('Paschal Johnpaul')
-        track = request.GET.get('backend')
+        track = request.GET.get('Backend')
 
         utc_time = datetime.datetime.utcnow()
 
         response = {
-            'slack_name': slack_name,
+            'slack_name': 'Paschal Johnpaul',
             'current_day': utc_time.strftime('%A'),
             'utc_time': utc_time.isoformat(),
-            'track': track,
-            'github_file_url': None,
+            'track': 'Backend',
+            'github_file_url': 'https://github.com/Paschal-ike/projectZ/tree/main/myproject',
             'github_repo_url': 'https://github.com/Paschal-ike/projectZ.git',
             'status_code': 200,
-        }
+            
+        } 
+        
+        if 'github_file_url' in request.GET:
+            response['github_file_url'] = request.GET['github_file_url']
+
+        if 'github_repo_url' in request.GET:
+            response['github_repo_url'] = request.GET['github_repo_url']
+
+        return JsonResponse(response)
 
         
 
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-#from django.contrib import admin
+from django.contrib import admin
 from django.urls import path
-from .views import ApiView
+from myapp.views import ApiView
+
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('', ApiView.as_view(), name='api'),
+    path('admin/', admin.site.urls),
+    path('api/', ApiView.as_view(), name='api'),
 ]
 
 
