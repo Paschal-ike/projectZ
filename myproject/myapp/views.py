0a6caf95b2b6c82d7e85a8bd2abfb94f6b97d7de
paschal-ike <paschalpaul268@gmail.com>
2023-09-11 01:19:46 +0100

    third commit

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,34 +1,26 @@
-from django.shortcuts import render
-from django.views.generic import View
 from django.http import JsonResponse
-import datetime
+from django.utils import timezone
 
-class ApiView(View):
-    def get(self, request, *args, **kwargs):
-        slack_name = request.GET.get('Paschal Johnpaul')
-        track = request.GET.get('Backend')
+def api_endpoint(request):
+    
+    slack_name = request.GET.get('slack_name', 'Paschal Johnpaul')
+    track = request.GET.get('track', 'Backend')
 
-        utc_time = datetime.datetime.utcnow()
+    # current day of the week
+    current_day = timezone.now().strftime("%A")
 
-        response = {
-            'slack_name': 'Paschal Johnpaul',
-            'current_day': utc_time.strftime('%A'),
-            'utc_time': utc_time.isoformat(),
-            'track': 'Backend',
-            'github_file_url': 'https://github.com/Paschal-ike/projectZ/tree/main/myproject',
-            'github_repo_url': 'https://github.com/Paschal-ike/projectZ.git',
-            'status_code': 200,
-            
-        } 
-        
-        if 'github_file_url' in request.GET:
-            response['github_file_url'] = request.GET['github_file_url']
+    #current UTC time
+    utc_time = timezone.now().strftime("%Y-%m-%dT%H:%M:%SZ")
 
-        if 'github_repo_url' in request.GET:
-            response['github_repo_url'] = request.GET['github_repo_url']
+    # JSON response
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": "github.com/Paschal-ike/projectZ/tree/main/myproject",
+        "github_repo_url": "https://github.com/Paschal-ike/projectZ.git",
+        "status_code": 200
+    }
 
-        return JsonResponse(response)
-
-        
-
-# Create your views here.
+    return JsonResponse(response_data)
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from myapp.views import ApiView
+from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', ApiView.as_view(), name='api'),
+    path('api/', views.api_endpoint, name='api_endpoint'),
 ]
+
+
 
 
 
